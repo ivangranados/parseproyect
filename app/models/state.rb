f256383697a78edcf218db9448fdad611fc51d71
ivangranados <ivan.granados@koombea.com>
2013-05-07 00:02:29 -0500

    model changes

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,22 @@
 class State < ActiveRecord::Base
   attr_accessible :content, :url
+
+
+def self.scrape_content
+	   
+	states = State.all 
+	states.each do |state|
+	 page = Nokogiri::HTML(RestClient.get("http://www.buycox.com/cox-cable-service/#{state.url}"))
+	 data = page.css('div#seocontentbody').to_s
+	 state.content = data
+	 "content of #{state.url} saved" if state.save
+	end	
+
+end
+
+
+
+
+
+
 end
